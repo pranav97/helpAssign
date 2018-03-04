7a4a33f785c1d18154d32fd8a53c868b6749b24d
ensj <limjunhyun@gmail.com>
2018-03-04 04:18:57 -0800

    Added fixes to database.

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,50 @@
+(function(){
+    const config = {
+        apiKey: "AIzaSyAmZ8lZEcvpDd73LxKVWbPXfCRJUZNEG4k",
+        authDomain: "project-kindling.firebaseapp.com",
+        databaseURL: "https://project-kindling.firebaseio.com",
+        storageBucket: "project-kindling.appspot.com",
+    };
 
-	var config = {
-    	apiKey: "AIzaSyAXNLXFvexDMw2_UXpP5q4aDYYG4xJcYWo",
-    	authDomain: "auth-2b8cc.firebaseapp.com",
-    	databaseURL: "https://auth-2b8cc.firebaseio.com",
-    	projectId: "auth-2b8cc",
-    	storageBucket: "auth-2b8cc.appspot.com",
-    	messagingSenderId: "119297236225"
-	};
-	firebase.initializeApp(config);
+    firebase.initializeApp(config);
+
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnRegister = document.getElementById('btnRegister');
+    const btnLogOut = document.getElementById('btnLogOut');
+
+    btnLogin.addEventListener('click', e =>{
+        const email = txtEmail.value
+        const password = txtPassword.value
+        const auth = firebase.auth()
+
+        const promise = auth.signInWithEmailAndPassword(email,password);
+        promise
+            .catch(e => console.log(e.message));
+    });
+    btnRegister.addEventListener('click', e =>{
+        const email = txtEmail.value
+        const password = txtPassword.value
+        const auth = firebase.auth()
+
+        const promise = auth.createUserWithEmailAndPassword(email,password);
+        promise 
+            .catch(e => console.log(e.message));
+    });
+
+    btnLogOut.addEventListener('click', e =>{
+        firebase.auth().signOut();
+    });
+
+    firebase.auth().onAuthStateChanged(firebaseUser =>{
+        if(firebaseUser){
+            console.log(firebaseUser);
+            alert('You are logged in!');
+            location.assign('index.html');
+        } else {
+            console.log('not logged in');
+            alert('You are not logged in!')
+        }
+    });
+}());
