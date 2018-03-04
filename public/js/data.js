a7ab16873ee77a71cc775a410908361b25a58b56
ensj <limjunhyun@gmail.com>
2018-03-04 06:14:04 -0800

    Added script in index to data.js instead

--- public/js/data.js
+++ public/js/data.js
@@ -1,26 +1,16 @@
 (function(){
-    const config = {
+    // Initialize Firebase
+      var config = {
         apiKey: "AIzaSyDD68a9OuEjCNeZMjXqvWvjKyN9GO1XpvU",
         authDomain: "taskassigner-13c49.firebaseapp.com",
         databaseURL: "https://taskassigner-13c49.firebaseio.com",
         projectId: "taskassigner-13c49",
         storageBucket: "taskassigner-13c49.appspot.com",
         messagingSenderId: "293878815857"
-    };
-
-    var database = firebase.database();
-
-    const locat = document.getElementById('loc');
-    const proble = document.getElementById('prob');
-    const urgen = document.getElementById('urg');
-    const toolsn = document.getElementById('tools');
-    const tasksn = document.getElementById('tasks');
-    const submit = document.getElementById('submit');
-
-    submit.addEventListener('click', e=>{
-        database.ref('/' + locat.value).set({
-            problem: proble.value,
-            urgency: urgen.value,
-        });
-    });
+      };
+      firebase.initializeApp(config);
+      var database = firebase.database();
+      
+      var dbref = database.ref().child('text');
+      dbref.on('value', snap=>big.innerText = "big" + " :  " + snap.val());
 }());
